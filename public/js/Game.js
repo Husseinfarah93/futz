2d15927cf22979fad4ae3600f13c00de3928ad88
Hussein Farah <husseinfarah93@gmail.com>
2017-03-04 18:49:22 -0500

    set up static files to be served and browserify for module loading

---
+++ public/js/Game.js
@@ -0,0 +1,1 @@
+// watchify /Users/Hussein/Desktop/testProjects/futz/public/js/Game.js -o /Users/Hussein/Desktop/testProjects/futz/public/js/bundle.js 
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 let express = require('express')
-let app = express() 
+let app = express(), path = require('path')
+
+
+app.use('/public', express.static(path.join(__dirname, '/public')))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
+
 
 app.listen(3000, () => {
   console.log('listening on port 3000')

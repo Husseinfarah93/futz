7f882e73a463a787f8bd1e76d0a9ba71e10e5fd9
Hussein Farah <husseinfarah93@gmail.com>
2017-03-04 17:28:48 -0500

    server responds with index.html

---
+++ index.js
@@ -0,0 +1,10 @@
+let express = require('express')
+let app = express() 
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(3000, () => {
+  console.log('listening on port 3000')
+})
